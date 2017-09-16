4af34182f15d06ea48f88b9030eeaa5916bac4e1
billcyz <billcyz@gmail.com>
2017-09-16 23:40:30 +1200

    elas unit testing template

---
+++ test/elas_unit.erl
@@ -0,0 +1,23 @@
+%% @author billcyz
+%% @doc @todo Add description to elas_unit.
+
+
+%% Unit (component) test template
+
+-module(elas_unit).
+
+-export([]).
+
+
+%% -----------------------------------------------------------------
+
+
+%% Bind project (find available project in data management console)
+%% Extract relative data
+bind_project(ProjectId) ->
+	1.
+
+%% Test function
+test(Fun) ->
+	2.
+
