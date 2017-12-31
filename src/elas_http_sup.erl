b79fcceed4e755cef4c60c3927930d68f1271e86
billcyz <billcyz@gmail.com>
2017-12-31 15:47:06 +1300

    31/12/2017

--- src/elas_http_sup.erl
+++ src/elas_http_sup.erl
@@ -8,14 +8,14 @@
 
 -export([start_link/0, start_link/1]).
 
--include("elas_include.hrl").
+%% -include("elas_include.hrl").
 
 %% -----------------------------------------------------------------
 
 %% Start http server supervisor
 -spec start_link() -> 'ok'.
 start_link() ->
-	start_link(?DEFAULT_HTTP_PORT).
+	start_link(8080).
 
 start_link(Port) ->
 	case is_integer(Port) of
@@ -25,7 +25,7 @@ start_link(Port) ->
 
 init([Port]) ->
 	{ok, Socket} = gen_tcp:listen(Port, [binary, {active, false},
-    {packet, http_bin}]),
+										 {packet, http_bin}]),
 	spawn_link(fun init_listener/0),
 	{ok, {{simple_one_for_one, 60, 3600}, 
 		  [{http_server, {elas_http, start_link, [Socket]},
